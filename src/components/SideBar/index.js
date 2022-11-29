c6491f30a812b3ba9b5d79b9cb773744d9d29f6a
Shami Sharma <shamisharma01@outlook.com>
2022-11-29 17:25:27 -0500

    Completed the sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const SideBar = () => (
     <div className='nav-bar'>
@@ -22,6 +23,13 @@ const SideBar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/shami0122">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
